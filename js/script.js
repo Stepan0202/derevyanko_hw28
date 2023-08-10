719bbd59c17993a4bb318c0870a77f36a2090cf3
Stepan0202 <stepan020293@gmail.com>
2023-08-11 01:13:28 +0300

    Problems with custom modal buttons

--- js/script.js
+++ js/script.js
@@ -1,40 +1,64 @@
 'use strict'
-
+//VARIABLES AND OBJECTS
 let currentData;
 //Object with common data (count of pages, nextPage, array of results etc)
 const data = {
     characters : [],
     planets : [],
     vehicles : [],
+    films: [],
+    starships: [],
+    species: [],
 }
 //Object with concrete results by types — arrays "result" from data. Fullfills in createCards function
 const dataResults = {
-    characters : [],
-    planets : [],
-    vehicles : [],
 }
 const details = document.querySelector("#detailsContainer");
 const detailsHeader = details.querySelector('.modal-header .modal-title');
-console.log(detailsHeader)
 const detailsBody = details.querySelector('.modal-body');
+const detailsClose = details.querySelector('.btn-close');
 const detailsObj = {
     container: details,
     header: detailsHeader,
     body: detailsBody,
+    btnClose: detailsClose,
     setHeader(string){
         this.header.innerHTML = string;
-        console.log(this.headet)
     },
     clearBody(){
         this.body.innerHTML = '';
     },
-    addToBody([...args]){
-        console.log(args)
-        args.forEach(el => this.body.appendChild(el));
+    addToBody(){
+        for(let i = 0; i < arguments.length; i++){
+            this.body.appendChild(arguments[i]);
+        }
+    },
+    setVisible(){
+        this.container.style = "display: block";
+
+        this.btnClose.addEventListener('click', this.setHidden);
+
+    },
+    setHidden(){
+        this.container.style = "display: none";
     }
 }
 const cardsPerPage = 10;
 const paginations = document.querySelectorAll('[aria-label="Page navigation"]')
+
+//Start
+for (const unit in data) {
+    if (Object.hasOwnProperty.call(data, unit)) {
+        const element = data[unit];
+        getAll(unit, element);
+    }
+}
+
+
+
+
+
+//Functions
 function getAll(type, array, url){
     let query;
     if (!url) query = fetch(`https://swapi.dev/api/${type == 'characters' ? 'people' : type}`);
@@ -55,8 +79,7 @@ function getAll(type, array, url){
                 cardContainer.addEventListener('click', processContainerClick);
             }
     })
-};
-
+}
 function createCards(array, type) {
     let results = [];
     array.forEach(object => {
@@ -64,21 +87,20 @@ function createCards(array, type) {
     });
     results = results.flat();
     results.forEach((obj, index) => {
-        console.log(obj.url);
         const photoIndex = obj.url.match(/(\d+)/gm);
-        console.log(photoIndex);
+        const name = obj.name ? obj.name : obj.title;
         obj.photo = `https://starwars-visualguide.com/assets/img/${type = type == 'people'? 'characters' : type}/${photoIndex}.jpg`;
         const card = `
         <div class="card"">
-            <div class="py-2"><img src="${obj.photo}" class="card-img-top" onerror="this.onerror=null;this.src='img/image_not_found.png';" alt="${obj.name}"></div>
+            <div class="py-2"><img src="${obj.photo}" class="card-img-top" onerror="this.onerror=null;this.src='img/image_not_found.png';" alt="${name}"></div>
             <div class="card-body">
-                <h5 class="card-title">${obj.name}</h5>
-                <a href="#" type="button" data-id="moreInfoButton" data-type="${type}" data-index="${index}" class="btn btn-primary data-objectInfo="[${index}, ${obj.name}]"" data-bs-toggle="modal" data-bs-target="#detailsContainer">More info</a>
+                <h5 class="card-title">${name}</h5>
+                <a href="#" type="button" data-id="moreInfoButton" data-type="${type}" data-index="${index}" class="btn btn-primary" data-objectInfo="[${index}, ${name}]" data-bs-target="#detailsContainer">More info</a>
             </div>
         </div>
         `
         obj.card = card;
-        obj.index = index;
+        obj.index = photoIndex;
     })
     dataResults[type] = results;
     return results;
@@ -109,6 +131,15 @@ function createPagination(count, elPerPage, cardContainer, type){
     pagination.addEventListener('click', processPagination);
     
 }
+
+function createUnitPhoto(type, index){
+    const unit = dataResults[type][index]
+}
+function appendChilds(container, elements){
+    elements.forEach(el => container.appendChild(el));
+}
+
+//Callbacks for event listeners
 function processPagination(e){
     if (e.target && !(e.target.querySelector('.disabled'))){
         const button = e.target;
@@ -170,44 +201,121 @@ function processPagination(e){
     }
 
 
-};
+}
 function processContainerClick(e){
     if (e.target.dataset.id == "moreInfoButton"){
         const targetData = e.target.dataset;
         const targetType = targetData.type;
         const targetIndex = targetData.index;
+        const unit = dataResults[targetType][targetIndex];
+
+        //regardless of type info: name, photo, etc
+        const name = unit.name ? unit.name : unit.title;
+        const photoSrc = unit.photo;
+        const photo = document.createElement('img');
+        const container = document.createElement('div');
+        const row = document.createElement('div');
+        const row2 = document.createElement('div');
+        const row3 = document.createElement('div'); 
+        const infoBlock = document.createElement('div');
+
+        container.classList.add('container');
+        container.appendChild(row);
+
+        row.classList.add('row-column-1', 'row-column-md-2', 'row');
+        row.appendChild(photo);
+        row.appendChild(infoBlock);
+        
+        infoBlock.classList.add('col-5');
+
+        photo.setAttribute('src', photoSrc);
+        photo.classList.add('col-7');
+        
+        detailsObj.setHeader(name);
+        detailsObj.clearBody();
+        detailsObj.addToBody(container);
+        detailsObj.setVisible();
+        
 
         switch (targetType){
             case "characters":
-                const name = dataResults['characters'].name;
-                const height = dataResults['characters'].height;
-                const mass = dataResults['characters'].mass;
-                const hair_color = dataResults['characters'].hair_color;
-                const skin_color = dataResults['characters'].skin_color;
-                const eye_color = dataResults['characters'].eye_color;
-                const birth_year = dataResults['characters'].birth_year;
-                const gender = dataResults['characters'].gender;
-                const homeworld = dataResults['characters'].homeworld;
-                const photo = dataResults['characters'].photo;
+                
+                const height =  document.createElement('p');
+                const mass =  document.createElement('p');
+                const hair_color = document.createElement('p'); 
+                const skin_color = document.createElement('p'); 
+                const eye_color =  document.createElement('p');
+                const birth_year = document.createElement('p'); 
+                const gender =  document.createElement('p');
+                const homeworld =  [document.createElement('p'), document.createElement('div'), unit.homeworld.match(/(\d+)/gm)];
+                
+                height.textContent = "Height: " + unit.height;        
+                mass.textContent = "Mass: " + unit.mass;
+                hair_color.textContent = "Hair color: " + unit.hair_color;
+                skin_color.textContent = "Skin color: " + unit.skin_color;
+                eye_color.textContent = "Eye color: " + unit.eye_color;
+                birth_year.textContent = "Birth year: " + unit.birth_year;
+                gender.textContent = "Gender: " + unit.gender;
+                homeworld[1].innerHTML = dataResults['planets'][homeworld[2]].card;
+                homeworld[1].classList.add('col-4');
+                homeworld[1].addEventListener('click', processContainerClick);
+                homeworld[0].textContent = "Homeworld:"
+                
+                
+                row2.classList.add('row-column-1', 'row-column-md-2', 'row');
+                row3.classList.add('row-column-1', 'row-column-md-2', 'row');
 
-                detailsObj.setHeader(name);
-                detailsObj.clearBody();
-                detailsObj.addToBody(photo);
+                
+                appendChilds(infoBlock, [height, mass, hair_color, skin_color, eye_color, birth_year, gender, homeworld[0], homeworld[1]]);
+                appendChilds(container, [row2, row3]);
+
+
                 break;
             case "vehicles":
 
+
                 break;
-            case "planet":
+            case "planets":
 
                 break;
             
+            case "films":
+
+                break;
+            case "starships":
+
+                break;
+            case "species":
+
+                break;
+
         }
-        detailsBody.innerHTML = dataResults[targetType][targetIndex].card;
     }
 }
-getAll('characters', data.characters);
-getAll('planets', data.planets);
-getAll('vehicles',data.vehicles);
 
 
+    // <div class="modal fade" id="detailsContainer" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="detailsContainerLabel" aria-hidden="true">
+    //     <div class="modal-dialog modal-dialog-centered modal-lg">
+    //     <div class="modal-content">
+    //         <div class="modal-header">
+    //         <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+    //         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    //         </div>
+    //         <div class="modal-body" id="moreInfo">
+    //         ...
+    //         </div>
+    //     </div>
+    //     </div>
+    // </div>
 
+//     <div class="modal fade show" id="detailsContainer" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="detailsContainerLabel" style="display: block;" aria-modal="true" role="dialog">
+//     <div class="modal-dialog modal-dialog-centered modal-lg">
+//       <div class="modal-content">
+//         <div class="modal-header">
+//           <h1 class="modal-title fs-5" id="staticBackdropLabel">Return of the Jedi</h1>
+//           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+//         </div>
+//         <div class="modal-body" id="moreInfo"><div class="container"><div class="row-column-1 row-column-md-2 row"><img src="https://starwars-visualguide.com/assets/img/films/3.jpg" class="col-7"><div class="col-5"></div></div></div></div>
+//       </div>
+//     </div>
+//   </div>
